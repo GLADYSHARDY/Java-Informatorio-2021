cce7c33bd6551a2a643a989903764b044a7d69fc
GLADYSHARDY <88208998+GLADYSHARDY@users.noreply.github.com>
2021-11-18 16:04:01 -0300

    Update Alumnos.java

--- EjercicioComplementariosLevel3/Alumnos.java
+++ EjercicioComplementariosLevel3/Alumnos.java
@@ -10,27 +10,21 @@ public class Alumnos {
         this.surname = surname;
         this.birthday = birthday;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
     public LocalDate getBirthday() {
         return birthday;
     }
-
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
